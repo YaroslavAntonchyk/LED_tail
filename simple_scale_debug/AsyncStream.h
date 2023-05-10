4468ecb4cad6e50b23d62d559e91e047ce0eeb9e
ant_robot <yaroslav.antonchik@gmail.com>
2023-05-10 16:41:35 +0200

    Added debug version of simple_scale

---
+++ simple_scale_debug/AsyncStream.h
@@ -0,0 +1,90 @@
+/*
+    Библиотека для асинхронного чтения объектов Stream (Serial итд)
+    Документация: 
+    GitHub: https://github.com/GyverLibs/AsyncStream
+    Возможности:
+    - Неблокирующее чтение в свой буфер
+    - Указание символа терминатора
+    
+    AlexGyver, alex@alexgyver.ru
+    https://alexgyver.ru/
+    MIT License
+
+    Версии:
+    v1.0 - релиз
+    v1.1 - исправлен баг
+*/
+
+#ifndef _AsyncStream_h
+#define _AsyncStream_h
+#include <Arduino.h>
+
+template < uint16_t SIZE >
+class AsyncStream 
+{
+public:
+    AsyncStream(Stream* port, uint8_t len = 4, uint16_t tout = 5) 
+    {
+        _port = port;
+        _tout = tout;
+        _len = len;
+    }
+    
+    // установить таймаут
+    void setTimeout(uint16_t tout) 
+    {
+        _tout = tout;
+    }
+
+    void setLen(uint8_t len)
+    {
+        _len = len;
+    }
+    
+    // данные приняты
+    bool available() 
+    {
+      if (_port -> available()) 
+      {
+        if (!_parseF) 
+        {
+          _parseF = true;
+          _count = 0;
+          _tmr = millis();
+        }
+        char ch = _port -> read();
+        if ((_count + 1) == _len)
+        {
+          // Serial.println("len");
+          buf[_count] = ch;
+          _parseF = false;
+          return true;
+        }
+        else if (_count < SIZE - 1) 
+        {
+          buf[_count++] = ch;
+        }
+        _tmr = millis();
+      }
+      if (_parseF && millis() - _tmr >= _tout) 
+      {
+        // Serial.println("time");
+        _parseF = false;
+        buf[_count] = '\0';
+        return true;
+      }
+      return false;
+    }
+    
+    // доступ к буферу
+    char buf[SIZE];
+
+private:
+    Stream* _port;
+    uint8_t _len;
+    uint16_t _tout, _count = 0;
+    uint32_t _tmr = 0;
+    bool _parseF = false;
+};
+
+#endif
--- simple_scale_debug/simple_scale_debug.ino
+++ simple_scale_debug/simple_scale_debug.ino
@@ -98,47 +98,45 @@ void loop()
       float raw = abs(LoadCell.getData());
       sample = sample*0.8f + raw*0.2f;
       state = sample > treshold;
+      Serial.println(sample);
       if (state)
         digitalWrite(ledPin, HIGH);
       else 
         digitalWrite(ledPin, LOW);
-      // Serial.print(raw);
-      // Serial.print(',');
-      // Serial.println(sample);
   }
-  if (serial.available()) 
-  {
-    Message rxMsg(serial.buf);
-    // size_t msgLength = Serial.readBytes((byte*)&rxMsg, sizeof(rxMsg));
+  // if (false && serial.available()) 
+  // {
+  //   Message rxMsg(serial.buf);
+  //   // size_t msgLength = Serial.readBytes((byte*)&rxMsg, sizeof(rxMsg));
 
-    byte crc = crc8_bytes((byte*)&rxMsg, sizeof(rxMsg));
-    // uncomment for debbuging
-    // Serial.print(static_cast<int>(rxMsg.id));
-    // Serial.print(rxMsg.state);
-    // Serial.print(rxMsg.color);
-    // Serial.print(crc == 0 ? "OK" : "F");
-    // Serial.println(rxMsg.id == DEVICE_ID);
-    if ((crc == 0) && (rxMsg.id == DEVICE_ID))
-    {
-      digitalWrite (EnTxPin, HIGH);
-      Message txMsg(DEVICE_ID, (state ? 'y' : 'n'), rxMsg.color);
-      txMsg.crc = crc8_bytes((byte*)&txMsg, sizeof(txMsg) - 1);
-      Serial.write((byte*)&txMsg, sizeof(txMsg));
-      Serial.flush();
-      digitalWrite(EnTxPin, LOW);
+  //   byte crc = crc8_bytes((byte*)&rxMsg, sizeof(rxMsg));
+  //   // uncomment for debbuging
+  //   // Serial.print(static_cast<int>(rxMsg.id));
+  //   // Serial.print(rxMsg.state);
+  //   // Serial.print(rxMsg.color);
+  //   // Serial.print(crc == 0 ? "OK" : "F");
+  //   // Serial.println(rxMsg.id == DEVICE_ID);
+  //   if ((crc == 0) && (rxMsg.id == DEVICE_ID))
+  //   {
+  //     digitalWrite (EnTxPin, HIGH);
+  //     Message txMsg(DEVICE_ID, (state ? 'y' : 'n'), rxMsg.color);
+  //     txMsg.crc = crc8_bytes((byte*)&txMsg, sizeof(txMsg) - 1);
+  //     Serial.write((byte*)&txMsg, sizeof(txMsg));
+  //     Serial.flush();
+  //     digitalWrite(EnTxPin, LOW);
 
-      if (rxMsg.color == 'g')
-      {
-        digitalWrite(ledPin, HIGH);
-        setLedColorRGB(0, MAX_BRIGHTNESS, 0); //green
-      }
-      else if(rxMsg.color == 'd')
-      {
-        digitalWrite(ledPin, LOW);
-        setLedColorRGB(MAX_BRIGHTNESS, 0, 0); //red 
-      }
-    }
-  }
+  //     if (rxMsg.color == 'g')
+  //     {
+  //       digitalWrite(ledPin, HIGH);
+  //       setLedColorRGB(0, MAX_BRIGHTNESS, 0); //green
+  //     }
+  //     else if(rxMsg.color == 'd')
+  //     {
+  //       digitalWrite(ledPin, LOW);
+  //       setLedColorRGB(MAX_BRIGHTNESS, 0, 0); //red 
+  //     }
+  //   }
+  // }
   // Serial.println(micros() - t);
 }
 
